51493d8cbf27b1e3c1884462d1512b81cd1184e3
Moges Tesema <moges2421@gmail.com>
2025-12-26 09:19:49 +0300

    Remove print statement for DATABASE_URL
    Remove print statement for DATABASE_URL.

--- utility/database/main.py
+++ utility/database/main.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DATABASE_URL = os.getenv("DATABASE_URL", "")
-print(DATABASE_URL)
+
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable not set!")
     
